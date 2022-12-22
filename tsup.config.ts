46ea5e950b3f316954efd022174fa9871ce3a301
Jannik Wibker <jannik.wibker@quantco.com>
2022-12-22 19:46:20 +0100

    initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: {
+    index: 'src/index.ts',
+  },
+  dts: true,
+  clean: true,
+  target: 'es2020',
+  format: ['esm'], // TODO: does gha require cjs?
+  sourcemap: true,
+  minify: false
+})
